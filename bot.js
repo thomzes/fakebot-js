f8f159cd41182d8396e0a671c34717adfb0f31fa
Tommy <thomz161@gmail.com>
2023-04-11 16:30:19 +0700

    updated

--- bot.js
+++ bot.js
@@ -3,16 +3,46 @@ const jawaban = document.getElementById('jawaban');
 
 let init = 0;
 
-const botSay = () => {
+const botSay = (data) => {
     return [
-        'Hallo perkenalkan saya Thomzbot, siapa nama kamu?'
+        'Hallo perkenalkan saya Thomzbot, siapa nama kamu?',
+        `Halo ${data?.nama}, berapa usia kamu?`,
+        `Ohh ${data?.usia}, hobi kamu apa?`,
+        `Sama dong kamu suka ${data?.hobi}, btw punya pacara ngga?`,
+        `Ohh ${data?.pacar}, ya udah kalau gitu, udahan ya?`
     ]
 }
 
 pertanyaan.innerHTML = botSay()[0];
 
 function botStart() {
-    console.log('starting bot');
+    init++
+    if (init === 1) {
+        setTimeout(botDelay({nama: jawaban.value}), 5500);
+    } else if (init === 2) {
+        setTimeout(botDelay({usia: jawaban.value}), 5500);
+    } else if (init === 3) {
+        setTimeout(botDelay({hobi: jawaban.value}), 5500);
+    } else if (init === 4) {
+        setTimeout(botDelay({pacar: jawaban.value}), 5500);
+    } else if (init === 5) {
+        finishing()
+    }else {
+        botEnd();
+    }
+    
+    function botDelay(jawabanUser) {
+        pertanyaan.innerHTML = botSay(jawabanUser)[init];
+    }
+
+    function finishing() {
+        console.log('Finish!');
+    }
+
+    function botEnd() {
+        console.log('Ended!')
+    }
+
 }
 
 
